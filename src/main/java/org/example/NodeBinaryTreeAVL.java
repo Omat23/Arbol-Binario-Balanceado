da62d876d8ab8d7c43cce419bf0f3512ce923d35
Omar Gonzalez <omendezcarreon@gmail.com>
2025-08-10 16:53:24 -0600

    Class NodeBinaryTreeAVL created

---
+++ src/main/java/org/example/NodeBinaryTreeAVL.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class NodeBinaryTreeAVL {
+
+    private int dataBinaryTreeAVL;
+    private int frequencyBalanceNodeBinaryTreeAVL;
+    private NodeBinaryTreeAVL sonLeftNodeBinaryTreeAVL;
+    private NodeBinaryTreeAVL sonrRightNodeBinaryTreeAVL;
+
+    public NodeBinaryTreeAVL(int dataBinaryTreeAVL) {
+        this.dataBinaryTreeAVL = dataBinaryTreeAVL;
+        this.frequencyBalanceNodeBinaryTreeAVL = 0;
+        this.sonLeftNodeBinaryTreeAVL = null;
+        this.sonrRightNodeBinaryTreeAVL = null;
+    }
+
+    public int getDataBinaryTreeAVL() {
+        return dataBinaryTreeAVL;
+    }
+
+    public int getFrequencyBalanceNodeBinaryTreeAVL() {
+        return frequencyBalanceNodeBinaryTreeAVL;
+    }
+
+    public void setFrequencyBalanceNodeBinaryTreeAVL(int frequencyBalanceNodeBinaryTreeAVL) {
+        this.frequencyBalanceNodeBinaryTreeAVL = frequencyBalanceNodeBinaryTreeAVL;
+    }
+
+    public NodeBinaryTreeAVL getSonLeftNodeBinaryTreeAVL() {
+        return sonLeftNodeBinaryTreeAVL;
+    }
+
+    public void setSonLeftNodeBinaryTreeAVL(NodeBinaryTreeAVL sonLeftNodeBinaryTreeAVL) {
+        this.sonLeftNodeBinaryTreeAVL = sonLeftNodeBinaryTreeAVL;
+    }
+
+    public NodeBinaryTreeAVL getSonrRightNodeBinaryTreeAVL() {
+        return sonrRightNodeBinaryTreeAVL;
+    }
+
+    public void setSonrRightNodeBinaryTreeAVL(NodeBinaryTreeAVL sonrRightNodeBinaryTreeAVL) {
+        this.sonrRightNodeBinaryTreeAVL = sonrRightNodeBinaryTreeAVL;
+    }
+}
